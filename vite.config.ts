51522814c032905f1e2787d1e0067657b90ea014
ReginYuan <49477488+ReginYuan@users.noreply.github.com>
2024-03-02 23:26:33 +0800

    feat: 修改代码

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { devPlugin, getReplacer } from "./plugins/devPlugin";
+import { buildPlugin } from "./plugins/buildPlugin";
 import optimizer from "vite-plugin-optimizer";
 
 export default defineConfig({
-  plugins: [optimizer(getReplacer()), devPlugin(), vue()],
+  plugins: [optimizer(getReplacer()), devPlugin(), vue(), buildPlugin()],
 });
